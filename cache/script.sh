362439df1bc8f6efaec01eb4f5b886f2a7ddce88
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2024-10-26 14:18:17 -0500

    Create script.sh

---
+++ cache/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install nano -y
+sudo apt install net-tools -y
+sudo apt-get install curl -y
+sudo apt install python3-pip -y
+sudo apt install tcpdump -y
+sudo pip install torch==1.11.0
+sudo pip install torchvision==0.12.0
+sudo pip install flwr==0.16.0
+sudo pip install numpy==1.22.1
+sudo pip install tqdm
+sudo pip install efficientnet_pytorch
+sudo pip install psutil
