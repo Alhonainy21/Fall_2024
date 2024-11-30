20c8ba49cdf6865610362e8a26804e1630b296ad
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2024-11-30 17:22:35 -0600

    Update script_kaggle.sh

--- script_kaggle.sh
+++ script_kaggle.sh
@@ -11,4 +11,4 @@ mkdir -p HAM10000_images
 cp HAM10000_images_part_1/* HAM10000_images/
 cp HAM10000_images_part_2/* HAM10000_images/
 #To check all are installed:
-ls -1 HAM10000_images | wc -l
+ls -1 HAM10000_images | wc -l  #10015
