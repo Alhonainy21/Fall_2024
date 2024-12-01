7b20201620e613d9749ceb66ca902d72d419b972
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2024-11-30 20:10:28 -0600

    Create script_ham_clouldlab.sh

---
+++ ham/script_ham_clouldlab.sh
@@ -0,0 +1,14 @@
+pip3 install kaggle
+nano ~/.bashrc
+#Add the following line
+export PATH=$PATH:/users/aga5h3/.local/bin
+source ~/.bashrc
+mkdir -p ~/.kaggle/
+mv kaggle.json ~/.kaggle/
+kaggle datasets download -d kmader/skin-cancer-mnist-ham10000
+unzip skin-cancer-mnist-ham10000.zip -d .
+mkdir -p HAM10000_images
+cp HAM10000_images_part_1/* HAM10000_images/
+cp HAM10000_images_part_2/* HAM10000_images/
+#To check all are installed:
+ls -1 HAM10000_images | wc -l  #10015
