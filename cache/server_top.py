cc2f0504028b3c0e8a974a8cb9281d013d3bfd42
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2024-12-11 10:56:26 -0600

    Update server_top.py

--- cache/server_top.py
+++ cache/server_top.py
@@ -13,7 +13,7 @@ from flwr.server.client_proxy import ClientProxy
 import logging
 import time
 
-####################################_FIFO cache raplacement_##################################
+####################################_TOP cache raplacement_##################################
 class CustomFedAvg(FedAvg):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
