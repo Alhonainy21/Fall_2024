8d67e489eff65778ece97dc61c158d1e52d34250
Alhonainy21 <92266474+Alhonainy21@users.noreply.github.com>
2025-02-17 17:34:44 +0300

    Create script_rasp25.sh

---
+++ script_rasp25.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get update -y
+apt-get install nano -y
+apt install net-tools -y
+apt-get install curl -y
+apt install python3-pip -y
+pip install torch==1.11.0
+pip install torchvision==0.12.0
+pip install flwr==0.16.0
+pip install numpy==1.22.1
+pip install tqdm
+pip install efficientnet_pytorch
+pip install psutil
